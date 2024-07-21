20c64807a6ffeb7e9e774db59f9760550bff1c10
Hryhorii Sukhikh <hryhorii.sukhikh@student.tuke.sk>
2024-07-22 00:11:55 +0200

    set script

---
+++ InstallKarl/InstallKarl.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+sudo apt update 
+if [ $? -eq 0 ]; then
+   echo ---update apt OK
+else
+   echo ---update apt FAIL 
+   exit 1
+fi
+sudo apt install git 
+if [ $? -eq 0 ]; then
+   echo ---install git OK
+else
+   echo ---install git FAIL
+   exit 1
+fi
+sudo apt-get install libncurses5-dev libncursesw5-dev > /dev/null
+if [ $? -eq 0 ]; then
+   echo ---install ncurses OK
+else
+   echo ---install ncurses FAIL
+   exit 1
+fi
+if [ -d ~/karel-the-robot ]; then
+   echo the folder karel-the-robot already exists
+   echo delete it and create a new one?
+   read -p "Y(yes)/N(no): " var
+   echo $var
+   if [[ "$var" == "Y" ]]; then
+      rm -rf ~/karel-the-robot
+   else
+      echo run the script again when you delete the folder
+      exit 1
+   fi
+fi
+git clone https://git.kpi.fei.tuke.sk/kpi/karel-the-robot.git ~/karel-the-robot> /dev/null
+if [ $? -eq 0 ]; then
+   echo ---install Karel Files OK
+else
+   echo ---install Karel Files FAIL
+   exit 1
+fi
+sudo apt install gcc cmake make gettext  pkg-config check > /dev/null
+if [ $? -eq 0 ]; then
+   echo ---install Karel Help Files OK
+else
+   echo ---install Karel Help Files FAIL
+   exit 1
+fi
+mkdir ~/karel-the-robot/build 
+cd ~/karel-the-robot/build 
+cmake .. > /dev/null
+sudo make
+sudo make install 
+make clean
+cd -
+rm -rf ~/karel-the-robot
+if [ $? -eq 0 ]; then
+   echo ---install Karel complete 
+else
+   echo ---install Karel error 
+   exit 1
+fi
+if [ -d ~/karel_project ]; then
+   echo the folder karel_project already exists
+   echo delete it and create a new one?
+   read -p "Y(yes)/N(no): " var
+   echo $var
+   if [[ "$var" == "Y" ]]; then
+      rm -rf ~/karel_project
+   else
+      echo run the script again when you delete the folder
+      exit 1
+   fi
+fi
+mkdir ~/karel_project
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cp -r $SCRIPT_DIR/src/karel_project/* ~/karel_project/
+echo "succeful karel_project install search info in karel_project/guide.txt"
+echo "\\/\\/\\/\\/\\/"
+echo "use <cd ~/karel_project>"
+cat ~/karel_project/guide.txt
---
+++ setup.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# -> ../info/day1.txt
+#
+check_and_install() {
+    local package_name="$1"
+    local install_command="$2"
+
+    # Проверка наличия пакета
+    if ! command -v "$package_name" &>/dev/null; then
+        echo "Пакет $package_name не найден. Установка..."
+        # Установка пакета через пакетный менеджер
+        if sudo "$install_command" install -y "$package_name" > /dev/null; then
+            echo "Пакет $package_name успешно установлен."
+        else
+            echo "Ошибка при установке пакета $package_name."
+            exit 1
+        fi
+    else
+        echo "Пакет $package_name уже установлен."
+    fi
+}
+
+packet_manager = "apt-get"
+
+check_and_install "git" "$packet_manager"
+check_and_install "make" "$packet_manager"
+check_and_install "gcc" "$packet_manager"
+check_and_install "valgrind" "$packet_manager"
+check_and_install "gdb" "$packet_manager"
+check_and_install "cgdb" "$packet_manager"
+check_and_install "cppcheck" "$packet_manager"
+
+read -p "Install Karl (y/n): " answer
+if [ "$answer"=="y" ]; then
+InstallKarl/InstallKarl.sh
+fi 
+read -p "Install Neovim (y/n): " answer
+if [ "$answer"=="y" ]; then
+InstallVim/InstallVim.sh
+fi 
